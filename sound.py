50fe534f7703395d1fc9df5555cb28ea04559c45
fanfreak2247 <keandrewinston@gmail.com>
2021-07-13 01:03:44 -0400

    Delete sound.py

--- sound.py
+++
@@ -1,7 +0,0 @@
-import pygame
-pygame.mixer.init()
-ball_blip = pygame.mixer.Sound("sound/ball_blip.wav")
-blip_paddle = pygame.mixer.Sound("sound/blip_paddle.wav")
-menu_click = pygame.mixer.Sound("sound/menu_click.wav")
-point_beep = pygame.mixer.Sound("sound/point_beep.wav")
-
